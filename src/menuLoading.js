22c0f0dbcf74312db01603c3834d38f045c5d638
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-05-21 17:03:36 -0600

    Finish code to generate menu & add styleForInit
    used the same css for the sake of not have more
    files and clutter. all is working well & working
    as its supposed to

--- src/menuLoading.js
+++ src/menuLoading.js
@@ -5,22 +5,28 @@ function menu(){
     content.innerHTML = '';
     let mainDiv = document.createElement('div');
     let header = document.createElement('h3');
-    let botttom = document.createElement('div');
-    let img = document.createElement('img');
-    let paragraph = document.createElement('p');
-
     header.innerText = 'Menu Restaurant';
     header.classList.add('conHeader');
-    img.setAttribute('alt', 'pic of restaurant');
-    paragraph.innerText = `Nestled in the heart of Willow Creek, The Enchanted Spoon offers a whimsical dining experience. The rustic decor,twinkling fairy lights, and lush indoor garden set a magical ambiance. Signature dishes include Dragonfire Steak and Elvenbread Delight, each crafted with locally sourced ingredients. Perfect for romantic dinners or family gatherings, it'swhere culinary dreams come true.`;
-
-
     mainDiv.appendChild(header);
-    botttom.appendChild(img);
-    botttom.appendChild(paragraph);
 
-    botttom.classList.add('bottom');
-    mainDiv.appendChild(botttom);
+
+
+
+    for(let i = 0; i < 5; i++){
+        let botttom = document.createElement('div');
+        let img = document.createElement('img');
+        let paragraph = document.createElement('p');
+        img.setAttribute('alt', 'pic of drink');
+        img.classList.add('drinkImg');
+        paragraph.innerText = `A velvety blend of rare Glimber root and forest herbs, served with a drizzle of truffle essence.`;
+        botttom.appendChild(img);
+        botttom.appendChild(paragraph);
+        botttom.classList.add('bottom');
+        botttom.classList.add('bottomMenu');
+
+        mainDiv.appendChild(botttom);
+    }
+    
     mainDiv.classList.add('mainSec');
 
     content.appendChild(mainDiv);
