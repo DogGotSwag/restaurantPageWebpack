2f5ce4cac3edf1891143c8c55a91c8ab07dd96cb
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-05-21 15:16:29 -0600

    Fix typo ib src/html had style to header
    had extra t in button element that didn't allow
    me to use it. Added some styling to make header
    look better

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import initial from './initialLoading';
 import './style.css';
 initial();
 
+
+
 let theHeader = document.querySelector('header');
 theHeader.classList.add('header');
+
+let nav = document.querySelector('nav');
+nav.classList.add('nav');
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(key => {
+    key.classList.add('buttons');
+});
